95cf8d20f857ec9890dd20e47809324ffcba97d5
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-27 09:51:49 -0300

    add MUI

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button as MButton } from "@mui/material";
+
+const Button: React.FC = () => {
+  return (
+    <MButton variant="contained" color="primary">
+      Click me
+    </MButton>
+  );
+};
+
+export default Button;
